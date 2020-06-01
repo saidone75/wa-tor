082b7a9dee2e6df9178654bc431dc0d0a25a0a07
saidone <saidone@saidone.org>
2020-06-01 18:29:20 +0200

    removed debug timer

--- src/wa_tor/board.cljs
+++ src/wa_tor/board.cljs
@@ -46,7 +46,7 @@
 (defn update-board []
   (if (:start @state)
     (let [prev-board (logic/sh-fi (:board @board))]
-      (time (swap! board assoc :board (logic/next-chronon @board)))
+      (swap! board assoc :board (logic/next-chronon @board))
       (if (= prev-board (logic/sh-fi (:board @board)))
         (let [area (* (:w @board) (:h @board))]
           (js/setTimeout #(swap! board assoc :board (logic/populate-board @board (quot area 10) (quot area 10))) 5000))))))
@@ -60,6 +60,6 @@
       (add-watch board :board #(draw-board))
       (let [area (* (:w @board) (:h @board))]
         (logic/populate-board @board (quot area 10) (quot area 10)))
-      (swap! state assoc :interval (js/setInterval update-board 1000) :speed 1)
-      (swap! state assoc :start true)))
+      (swap! state assoc :start true)
+      (swap! state assoc :interval (js/setInterval update-board 250) :speed 1)))
   state)
