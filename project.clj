072cb57f3cba2ee560ff51287b02cca3b17c057a
saidone <saidone@saidone.org>
2020-06-26 11:21:12 +0200

    updated some dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.764"]
+                 [org.clojure/clojurescript "1.10.773"]
                  [reagent "1.0.0-alpha2"]]
 
   :source-paths ["src"]
@@ -17,6 +17,6 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "wa-tor.test-runner"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.8"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.9"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    }})
