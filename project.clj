19245c7203cb61bedc05049ae7fae3983126f9bc
saidone <saidone@saidone.org>
2021-02-24 12:16:19 +0100

    updated clojure dep

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/clojurescript "1.10.773"]
                  [reagent "1.0.0"]]
 
