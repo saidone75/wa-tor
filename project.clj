20e03dbd1685db5da9347a9bfd4383e82c29b026
saidone <saidone@saidone.org>
2021-03-03 09:05:33 +0100

    new build configuration

--- project.clj
+++ project.clj
@@ -13,9 +13,8 @@
   :source-paths ["src"]
 
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
-            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
-            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "wa-tor.test-runner"]}
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "wa-tor" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "wa-tor"]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.12"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
--- test/wa_tor/core_test.cljs
+++
@@ -1,3 +0,0 @@
-(ns wa-tor.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]))
