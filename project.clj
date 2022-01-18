7f76d88ba3d5ee21e376b85523d9397d88596743
saidone <saidone@saidone.org>
2022-01-18 09:04:40 +0100

    update ClojureScript to 1.11.4, react to 18.0.0-rc.0-0

--- project.clj
+++ project.clj
@@ -12,10 +12,10 @@
              :archive {:format :tgz :root-dir ""}}
 
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.914"]
+                 [org.clojure/clojurescript "1.11.4"]
                  [reagent "1.1.0"]
-                 [cljsjs/react "17.0.2-0"]
-                 [cljsjs/react-dom "17.0.2-0"]]
+                 [cljsjs/react "18.0.0-rc.0-0"]
+                 [cljsjs/react-dom "18.0.0-rc.0-0"]]
 
   :source-paths ["src"]
 
