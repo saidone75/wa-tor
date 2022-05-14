997c9918c98ec29404e13bf16da396993424bd12
saidone <saidone@saidone.org>
2022-05-14 08:17:16 +0200

    update ClojureScript to 1.11.51, figwheel-main to 0.2.17

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
              :archive {:format :tgz :root-dir ""}}
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.11.4"]
+                 [org.clojure/clojurescript "1.11.51"]
                  [reagent "1.1.1"]
                  [cljsjs/react "18.0.0-rc.0-0"]
                  [cljsjs/react-dom "18.0.0-rc.0-0"]]
@@ -27,5 +27,5 @@
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "wa-tor" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "wa-tor"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.16"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.17"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
